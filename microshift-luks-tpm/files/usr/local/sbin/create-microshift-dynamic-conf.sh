884e515d21b8357692666e0ebbe1d999325ab43d
Luis Arizmendi <luis.ariz@gmail.com>
2025-11-04 11:54:04 +0100

    ushift luks tpm

---
+++ microshift-luks-tpm/files/usr/local/sbin/create-microshift-dynamic-conf.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CONFIG_DIR="/etc/microshift/config.d"
+CONFIG_FILE="${CONFIG_DIR}/99-dyn-values.yaml"
+
+mkdir -p "${CONFIG_DIR}"
+
+HOSTNAME=$(hostname -f)
+
+IP=$(ip -4 route get 1.1.1.1 | awk '/src/ {print $7; exit}')
+
+if [[ -z "$HOSTNAME" || -z "$IP" ]]; then
+    echo "Error: Unable to determine hostname or IP" >&2
+    exit 1
+fi
+
+# Write the YAML config
+cat > "${CONFIG_FILE}" <<EOF
+apiServer:
+  subjectAltNames:
+    - microshift.lablocal
+    - ${HOSTNAME}
+dns:
+  baseDomain: ${IP}.nip.io
+EOF
+
+chmod 644 "${CONFIG_FILE}"
+echo "Created ${CONFIG_FILE} with hostname=${HOSTNAME}, IP=${IP}"
+
+systemctl restart microshift
