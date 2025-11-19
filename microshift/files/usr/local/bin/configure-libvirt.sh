d733f616f2a30e77d3141492e0968c8c7af6cd14
Luis Arizmendi <luis.ariz@gmail.com>
2025-11-19 12:12:07 +0100

    flightctl hooks

--- microshift/files/usr/local/bin/configure-libvirt.sh
+++ microshift/files/usr/local/bin/configure-libvirt.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-virsh pool-define /etc/libvirt/storage/default.xml
-virsh pool-autostart default
-virsh pool-start default
+export LIBVIRT_DEFAULT_URI=qemu:///system
+virsh  net-undefine default
+virsh net-define /etc/libvirt/network/default.xml
+virsh net-start default
+virsh net-autostart default
+for i in $(ls /etc/libvirt/qemu/*.xml); do virsh define $i ;done
+
