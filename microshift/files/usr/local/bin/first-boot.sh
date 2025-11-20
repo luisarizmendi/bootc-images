71efdf7001459e3772ab818e877b6552b8e81263
Luis Arizmendi <luis.ariz@gmail.com>
2025-11-20 10:45:54 +0100

    fix boot

--- microshift/files/usr/local/bin/first-boot.sh
+++ microshift/files/usr/local/bin/first-boot.sh
@@ -15,12 +15,6 @@ until bash /usr/local/bin/get-files.sh; do
   sleep 5
 done
 
-## COCKPIT
-echo "Configuring SELinux for Cockpit..."
-#cd /root
-#ausearch -c 'cockpit-session' --raw | audit2allow -M my-cockpitsession
-#semodule -X 300 -i my-cockpitsession.pp
-
 ## LIBVIRT
 echo "Configuring libvirt..."
 export LIBVIRT_DEFAULT_URI=qemu:///system
@@ -35,5 +29,5 @@ virsh pool-autostart cloud-init
 for i in $(ls /etc/libvirt/qemu/*.xml); do virsh define $i ;done
 for i in $(virsh list --all --name); do systemctl start libvirt-vm@${i}.service && systemctl enable libvirt-vm@${i}.service ; virsh autostart $i ; done
 
-
+touch /var/lib/firstboot.done
 exit 0
