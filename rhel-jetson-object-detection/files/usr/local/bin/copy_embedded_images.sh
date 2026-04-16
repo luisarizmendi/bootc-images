1dd343e4a68278f4caeb99a254dd6b7a74f861a6
Luis Arizmendi <luis.ariz@gmail.com>
2026-04-16 11:35:53 +0200

    embedded images

--- rhel-jetson-object-detection/files/usr/local/bin/copy_embedded_images.sh
+++ rhel-jetson-object-detection/files/usr/local/bin/copy_embedded_images.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
-mkdir -p /var/lib/shared/overlay-images \
-         /var/lib/shared/overlay-layers \
-         /var/lib/shared/vfs-images \
-         /var/lib/shared/vfs-layers
-touch /var/lib/shared/libpod/container_api.sqlite 2>/dev/null || true
+
+# created with tmpfile
+#mkdir -p /var/lib/shared/overlay-images \
+#         /var/lib/shared/overlay-layers \
+#         /var/lib/shared/vfs-images \
+#         /var/lib/shared/vfs-layers
+
+#touch /var/lib/shared/libpod/container_api.sqlite 2>/dev/null || true
+
 while IFS="," read -r image sha
 do
     skopeo copy \
@@ -12,3 +16,8 @@ do
         dir:/usr/lib/containers-image-cache/$sha \
         containers-storage:[overlay@/var/lib/shared+/run/containers/storage]$image
 done < /usr/lib/containers-image-cache/mapping.txt
+
+# Make the shared store readable by the cstore group
+chown -R root:cstore /var/lib/shared/
+find /var/lib/shared/ -type f -exec chmod 640 {} \;
+find /var/lib/shared/ -type d -exec chmod 750 {} \;
