65b34bf51553eb778d2912cee901a8ca80a094a5
Luis Arizmendi <luis.ariz@gmail.com>
2026-04-17 12:45:24 +0200

    changes inference

--- rhel-jetson-object-detection/files/usr/local/bin/copy_embedded_images.sh
+++ rhel-jetson-object-detection/files/usr/local/bin/copy_embedded_images.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
+
 set -euxo pipefail
 
-# created with tmpfile
-#mkdir -p /var/lib/shared/overlay-images \
-#         /var/lib/shared/overlay-layers \
-#         /var/lib/shared/vfs-images \
-#         /var/lib/shared/vfs-layers
-
-#touch /var/lib/shared/libpod/container_api.sqlite 2>/dev/null || true
-
-while IFS="," read -r image sha
+while IFS="," read -r image sha target_user
 do
-    skopeo copy \
-        --preserve-digests \
+  if [[ -n "$target_user" ]]; then
+    runuser -l "$target_user" -c \
+      "skopeo copy --preserve-digests \
         dir:/usr/lib/containers-image-cache/$sha \
-        containers-storage:[overlay@/var/lib/shared+/run/containers/storage]$image
+        containers-storage:$image"
+  else
+    skopeo copy --preserve-digests \
+      dir:/usr/lib/containers-image-cache/$sha \
+      containers-storage:$image
+  fi
 done < /usr/lib/containers-image-cache/mapping.txt
-
-# Make the shared store readable by the cstore group
-chown -R root:cstore /var/lib/shared/
-find /var/lib/shared/ -type f -exec chmod 640 {} \;
-find /var/lib/shared/ -type d -exec chmod 750 {} \;
--- rhel-jetson-object-detection/files/usr/local/bin/embed_image.sh
+++ rhel-jetson-object-detection/files/usr/local/bin/embed_image.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+
 set -euxo pipefail
+
 image=$1
+shift
+
+target_user=""
+additional_copy_args=""
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --user)
+      target_user="$2"
+      shift 2
+      ;;
+    *)
+      additional_copy_args="$additional_copy_args $1"
+      shift
+      ;;
+  esac
+done
+
 mkdir -p /usr/lib/containers-image-cache
 sha=$(echo "$image" | sha256sum | awk '{ print $1 }')
-skopeo copy --preserve-digests docker://$image dir:/usr/lib/containers-image-cache/$sha
-echo "$image,$sha" >> /usr/lib/containers-image-cache/mapping.txt
+skopeo copy $additional_copy_args --preserve-digests docker://$image dir:/usr/lib/containers-image-cache/$sha
+echo "$image,$sha,$target_user" >> /usr/lib/containers-image-cache/mapping.txt
