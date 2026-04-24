4ef65752339a881c2a05d4c1c7f6c29d2f5f728f
Luis Arizmendi <luis.ariz@gmail.com>
2026-04-24 14:13:24 +0200

    remove test

--- rhel-test/README.md
+++
@@ -1,5 +0,0 @@
-# rhel bootc image
-
-This is a clean RHEL image with a simple first boot script. It has three versions, v2 adds 'tmux' package and v3 uses RHEL 10 instead RHEL 9.
-
-Associated ISO is created based on v1 to show how upgrades work.
--- rhel-test/files/usr/local/bin/first-boot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-###############################################################
-## HOSTNAME ###################################################
-echo "Configuring hostname..."
-/usr/local/bin/set-hostname-from-mac.sh
-
-
