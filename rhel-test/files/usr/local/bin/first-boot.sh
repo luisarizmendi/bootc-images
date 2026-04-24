b417260c817738c2163dad118b0577809833b5f3
Luis Arizmendi <luis.ariz@gmail.com>
2026-04-24 10:04:34 +0200

    rhel test

---
+++ rhel-test/files/usr/local/bin/first-boot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+###############################################################
+## HOSTNAME ###################################################
+echo "Configuring hostname..."
+/usr/local/bin/set-hostname-from-mac.sh
+
+
